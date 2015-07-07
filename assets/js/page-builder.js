bf9d71036548e96e1f170fa542a0e149b25a7d05
Chris Reynolds <chris@jazzsequence.com>
2015-07-07 16:33:25 -0600

    show the next row if the button is clicked

--- assets/js/page-builder.js
+++ assets/js/page-builder.js
@@ -1,5 +1,10 @@
-jQuery('.cmb-td option[selected="selected"]').each(function(index){
+var selected = jQuery('.cmb-td option[selected="selected"]');
+selected.each(function(index){
 	if ( 'none' == jQuery(this).val() ) {
-		jQuery(this).parent().parent().parent().hide()
+		jQuery(this).parent().parent().parent().hide();
+		jQuery('button.cmb-add-row-button').click(function(){
+			jQuery('.empty-row.hidden').show();
+
+		});
 	}
 });
