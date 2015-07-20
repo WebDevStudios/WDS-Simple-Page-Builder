bc3c8286d7a035f33fbc896cb4dc36025c6242ba
Chris Reynolds <chris@jazzsequence.com>
2015-07-20 12:29:53 -0600

    version bump

--- wds-simple-page-builder.php
+++ wds-simple-page-builder.php
@@ -5,7 +5,7 @@
  * Description: Uses existing template parts in the currently-active theme to build a customized page with rearrangeable elements.
  * Author: WebDevStudios
  * Author URI: http://webdevstudios.com
- * Version: 1.4.2
+ * Version: 1.5
  * License: GPLv2
  */
 
